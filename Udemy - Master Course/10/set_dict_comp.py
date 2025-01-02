2577ca81391d08facc7bb88144a1b6e09ed8f6c4
NetItUp-MatteoSala <matteo.sala@netitup.it>
2025-01-02 11:45:02 +0100

    timer

--- Udemy - Master Course/10/set_dict_comp.py
+++ Udemy - Master Course/10/set_dict_comp.py
@@ -1,1 +1,37 @@
+a = "stringa con delle parole"
+vocali = {i for i in a if i in 'aeiou'}
+
+print(sorted(vocali))
+
+x = [1,4,7,9]
+y = {i: 5 for i in x}
+print(y)
+
+import sys
+
+s = sum([i*i for i in range(1000)])
+print(s)
+print(sys.getsizeof([i*i for i in range(1000)]))
  
+#generatore - unica differenza sono le ()
+g = sum((i*i for i in range(1000)))
+print(g)
+print(sys.getsizeof((i*i for i in range(1000))))
+
+
+# avviare un timer
+from timeit import default_timer as timer
+
+start = timer()
+t = [i*i for i in range(1_000_000)]
+end = timer()
+print(end-start)
+
+# avviare un timer con ciclo forstart = timer()
+start = timer()
+a = []
+for i in range(1_000_000):
+    a.append(i*i)
+end = timer()
+
+print(end-start)
