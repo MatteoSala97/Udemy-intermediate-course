419ef8606ee7765b56ce5d29906efde77c5e40a5
NetItUp-MatteoSala <matteo.sala@netitup.it>
2025-01-02 11:27:06 +0100

    uso dei set

---
+++ Udemy - Master Course/08/set.py
@@ -0,0 +1,10 @@
+# i set sono univoci e non hanno index
+
+tools = {"hammer", "screwdriver", "screw"}
+
+tools2= set()
+
+my_tools = {"hammer"}
+
+if my_tools.issubset(tools):
+    print('Tutti i miei attrezzi fanno parte del set tools')
