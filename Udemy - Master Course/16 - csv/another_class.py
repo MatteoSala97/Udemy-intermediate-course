523073119d40ba528f3d58b0ca460aec7d8f9544
NetItUp-MatteoSala <matteo.sala@netitup.it>
2025-01-03 10:18:46 +0100

    implemented csv and created storage from csv

--- Udemy - Master Course/16 - csv/another_class.py
+++ Udemy - Master Course/16 - csv/another_class.py
@@ -1,4 +1,5 @@
 import csv
+
 class Item:
     #attributi di classe 
     sconto = 0.8
@@ -31,7 +32,7 @@ class Item:
         
     @classmethod #decoratore per aprire il file
     def crea_items_da_csv(cls):
-        with open('items.csv', 'r') as csvfile:
+        with open(r'C:\Users\matteo.sala\Documents\Udemy Courses\Udemy - Master Course\16 - csv\items.csv', 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             items = list(reader)
             
@@ -41,15 +42,18 @@ class Item:
                     price=float(i.get('price')), 
                     quantity=int(i.get('quantity')),
                 )
+                
+Item.crea_items_da_csv()
+# print(Item.archivio)
             
 
 item1 = Item("Penna", 1.5, 10)
 item2 = Item("Quaderno", 3, 5)  
 item3 = Item("Gomma", 1)
 
-print(f"Il prezzo totale dell'item {item1.get_name()} è {item1.prezzo_totale()}")
-print(f"Il prezzo totale dell'item {item2.get_name()} è {item2.prezzo_totale()}")
-print(f"Il prezzo totale dell'item {item3.get_name()} è {item3.prezzo_totale()}")
+# print(f"Il prezzo totale dell'item {item1.get_name()} è {item1.prezzo_totale()}")
+# print(f"Il prezzo totale dell'item {item2.get_name()} è {item2.prezzo_totale()}")
+# print(f"Il prezzo totale dell'item {item3.get_name()} è {item3.prezzo_totale()}")
 
 # print(Item.sconto)
 # print(item1.sconto)
@@ -57,10 +61,10 @@ print(f"Il prezzo totale dell'item {item3.get_name()} è {item3.prezzo_totale()}
 # print(Item.__dict__) #Restituisce tutti gli attributi a livello di classe
 # print(item1.__dict__) #Restituisce tutti gli attributi a livello di istanza 
 
-item3.applica_sconto()
-print(f"Il prezzo scontato dell'item {item3.get_name()} è {item3.prezzo_totale()}")
+# item3.applica_sconto()
+# print(f"Il prezzo scontato dell'item {item3.get_name()} è {item3.prezzo_totale()}")
 
-print(Item.archivio) 
+# print(Item.archivio) 
 
 for i in Item.archivio:
     print(f"Nome: {i.get_name()}, Prezzo: {i._price}, Quantità: {i._quantity}")
